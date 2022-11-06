38c2522a9b44e993a4053db8b15d2059875fe400
Владимир <bf.karatel@gmail.com>
2022-11-06 19:55:11 +0800

    0. Что такое инкапсуляция. [#3818 #358289]

--- src/main/java/ru/job4j/encapsulation/Config.java
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -18,7 +18,7 @@ public class Config {
         System.out.println(position);
     }
 
-    public String search(String key) {
+    private String search(String key) {
         return key;
     }
 }
