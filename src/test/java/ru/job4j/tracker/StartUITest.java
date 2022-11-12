979953064f034f8d96fcaebcffd9bef02c16bea2
Владимир <bf.karatel@gmail.com>
2022-11-12 13:17:24 +0800

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,9 +1,141 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
+
+    @Test
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+    Output out = new StubOutput();
+    Tracker tracker = new Tracker();
+    Item one = tracker.add(new Item("test1"));
+    String replaceName = "New Test Name";
+    Input in = new StubInput(
+            new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+    );
+    UserAction[] actions = new UserAction[] {
+            new ReplaceAction(out),
+            new ExitAction(out)
+    };
+    new StartUI(out).init(in, tracker, actions);
+    assertThat(out.toString()).isEqualTo(
+            "Menu:" + System.lineSeparator()
+                    + "0. Edit item" + System.lineSeparator()
+                    + "1. Exit Program" + System.lineSeparator()
+                    + "=== Edit Item ===" + System.lineSeparator()
+                    + "Request changed successfully." + System.lineSeparator()
+                    + "Menu:" + System.lineSeparator()
+                    + "0. Edit item" + System.lineSeparator()
+                    + "1. Exit Program" + System.lineSeparator()
+                    + "Exit Program" + System.lineSeparator()
+    );
+    }
+
+    @Test
+    public void whenFindAllActionTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        UserAction[] actions = new UserAction[] {
+                new ShowAllAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + System.lineSeparator()
+                        + "0. Show all items" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "=== Show all items ===" + System.lineSeparator()
+                        + "Item{"
+                        + "id=" + one.getId()
+                        + ", name='" + one.getName() + '\''
+                        + ", created=" + one.getCreate().format(formatter)
+                        + '}' + System.lineSeparator()
+                        + "Item{"
+                        + "id=" + two.getId()
+                        + ", name='" + two.getName() + '\''
+                        + ", created=" + two.getCreate().format(formatter)
+                        + '}' + System.lineSeparator()
+                        + "Menu:" + System.lineSeparator()
+                        + "0. Show all items" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "Exit Program" + System.lineSeparator()
+        );
+    }
+
+    @Test
+    public void whenFindByNameActionTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+        Input in = new StubInput(
+                new String[] {"0", one.getName(), "1"}
+        );
+        UserAction[] actions = new UserAction[] {
+                new FindByNameAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + System.lineSeparator()
+                        + "0. Find items by name" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "=== Find Item by name===" + System.lineSeparator()
+                        + "Item{"
+                        + "id=" + one.getId()
+                        + ", name='" + one.getName() + '\''
+                        + ", created=" + one.getCreate().format(formatter)
+                        + '}' + System.lineSeparator()
+                        + "Menu:" + System.lineSeparator()
+                        + "0. Find items by name" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "Exit Program" + System.lineSeparator()
+        );
+    }
+
+    @Test
+    public void whenFindByIdActionTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(two.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[] {
+                new FindByIdAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + System.lineSeparator()
+                        + "0. Find item by id" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "=== Find Item by id===" + System.lineSeparator()
+                        + "Item{"
+                        + "id=" + two.getId()
+                        + ", name='" + two.getName() + '\''
+                        + ", created=" + two.getCreate().format(formatter)
+                        + '}' + System.lineSeparator()
+                        + "Menu:" + System.lineSeparator()
+                        + "0. Find item by id" + System.lineSeparator()
+                        + "1. Exit Program" + System.lineSeparator()
+                        + "Exit Program" + System.lineSeparator()
+        );
+    }
+
     @Test
     public void whenCreateItem() {
         Output out = new StubOutput();
@@ -13,7 +145,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(out),
-                new Exit()
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
@@ -33,7 +165,7 @@ public class StartUITest {
 
         UserAction[] actions = {
                 new ReplaceAction(out),
-                new Exit()
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
@@ -51,7 +183,7 @@ public class StartUITest {
         );
         UserAction[] actions = {
                 new DeleteAction(out),
-                new Exit()
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
@@ -65,12 +197,13 @@ public class StartUITest {
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new Exit()
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString()).isEqualTo(
                 "Menu:" + System.lineSeparator()
-                        + "0. Exit" + System.lineSeparator()
+                        + "0. Exit Program" + System.lineSeparator()
+                        + "Exit Program" + System.lineSeparator()
         );
     }
 }
