8079bb1830bba97818bd74e90ccb56a05a275678
Владимир <bf.karatel@gmail.com>
2022-11-26 16:42:57 +0800

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizen = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!this.citizen.containsKey(citizen.getPassport())) {
+            this.citizen.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen search = null;
+        for (String key : citizen.keySet()) {
+            if (passport.equals(citizen.get(key).getPassport())) {
+                search = citizen.get(key);
+                break;
+            }
+        }
+        return search;
+    }
+}
